81f56e6ee2ce7f87ecab75f017d1eaded002a492
edidada <1664884095@qq.com>
2018-11-10 19:13:14 +0800

    add filter

---
+++ src/main/java/cn/edidada/MyGenericServlet.java
@@ -0,0 +1,14 @@
+package cn.edidada;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class MyGenericServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/cn/edidada/MyRequestDispatcher.java
@@ -0,0 +1,19 @@
+package cn.edidada;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class MyRequestDispatcher implements RequestDispatcher {
+    @Override
+    public void forward(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void include(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+    }
+}
