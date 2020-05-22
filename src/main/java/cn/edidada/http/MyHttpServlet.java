50eb70720cf4fd5ae521c81859e381df6285c539
edidada <sandisks555@gmail.com>
2020-05-22 14:53:23 +0800

    add MyHttpServlet

--- src/main/java/cn/edidada/http/MyHttpServlet.java
+++ src/main/java/cn/edidada/http/MyHttpServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class MyHttpServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doPost(req, resp);
